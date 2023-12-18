b38b0c591e2b860d7e165ea6a796a11a3fcc67f5
Daniel Sayer <daniel.sayer01@gmail.com>
2023-12-18 19:11:05 +1000

    fix: await the db call

--- src/server/api/routers/notes/route.ts
+++ src/server/api/routers/notes/route.ts
@@ -14,10 +14,9 @@ export const notesRouter = createTRPCRouter({
     return createdNote.id
   }),
   getUserNotes: protectedProcedure.query(async ({ ctx }) => {
-    console.log(ctx.session)
     const userId = ctx.session.user.id
 
-    return ctx.db.note.findMany({
+    return await ctx.db.note.findMany({
       where: {
         authorId: userId,
       },
